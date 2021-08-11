4bf27c14440164496a93776476b1cb48c0b7f330
Peter Donovan <peter@darkfusion.io>
2021-08-11 16:51:21 -0400

    Replace Material UI with custom CSS (PostCSS) Removed imports related to `@material-ui` component library to prepare for the migration to custom CSS styles.
    Removed theme object and other artifacts related to Material UI.
    
    Restructured JSX to allow for a smoother transition to CSS.
    
    Added a `Board` component that will serve as the central presentation
    component for the application. The `Board` component is responsible for
    displaying the Kanban board itself, the board's `List` components and
    the `Ticket` components that will live inside of the `List`s.

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,29 @@
-import { CssBaseline } from '@material-ui/core';
-import { ThemeProvider } from '@material-ui/core/styles';
 import React from 'react';
+import { Board } from './components/Board';
 
 import { Footer } from './layout/Footer';
 import { Header } from './layout/Header';
 import { MainContent } from './layout/MainContent';
 import { Sidebar } from './layout/Sidebar';
 
-import { theme } from './theme';
-
-export const App = (): JSX.Element => {
+export const App: React.FC = (): JSX.Element => {
   return (
     <React.Fragment>
-      <ThemeProvider theme={theme}>
-        <CssBaseline />
-        <Header />
-        <MainContent>
-          <section>This will hold the main content of the app</section>
-          <Sidebar>Sidebar will go here</Sidebar>
-        </MainContent>
-        <Footer>
-          <p>Designed and built by Peter Donovan</p>
-          <p>If you would like to see more of my work, please visit:&nbsp;</p>
-          <small>&copy; {new Date().getFullYear()} &mdash;&nbsp;</small>
-          <a href="https://darkfusion.io/" target="_blank" rel="noopener noreferrer">
-            Dark Fusion Systems
-          </a>
-          . All rights reserved.
-        </Footer>
-      </ThemeProvider>
+      <Header />
+      <MainContent>
+        <Sidebar>Sidebar will go here</Sidebar>
+        <section>
+          <Board />
+        </section>
+      </MainContent>
+      <Footer>
+        <small>If you would like to see more of our work, please visit us at:&nbsp;</small>
+        <small>&copy; {new Date().getFullYear()} &mdash;&nbsp;</small>
+        <a href="https://darkfusion.io/" target="_blank" rel="noopener noreferrer">
+          Dark Fusion Systems
+        </a>
+        . All rights reserved.
+      </Footer>
     </React.Fragment>
   );
 };
